320a4484ae98096ae18806de18486fcdaf467922
DaniilElmirov <elmirovwork@gmail.com>
2023-01-24 17:12:32 +0700

    Scripts

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,305 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameScript : MonoBehaviour
+{
+    public static int borderWidth = 11; 
+    public static int borderHeight = 24;  
+    public static int borderLimit = 21;  
+
+    public static Transform[,] border = new Transform[borderWidth, borderHeight];
+
+    public int scoreOneLine = 40;
+    public int scoreTwoLine = 100;
+    public int scoreThreeLine = 300;
+    public int scoreFourLine = 1200;
+
+    private int numberOfRowsThisTurn = 0;
+
+    public Text hud_score;
+    public Text hud_level;
+
+    public static int currentScore;
+
+    float fallSpeedGame;
+
+    void Start()
+    {
+        currentScore = 0;
+        SpawnNextTetramino();
+    }
+
+    public void UpdateUI()
+    {
+        hud_score.text = currentScore.ToString();
+
+        if(fallSpeedGame == 0.5f)
+        {
+            hud_level.text = 1.ToString();
+        }
+        else if(fallSpeedGame == 0.3f)
+        {
+            hud_level.text = 2.ToString();
+        }
+        else if (fallSpeedGame == 0.1f)
+        {
+            hud_level.text = 3.ToString();
+        }
+    }
+
+    public void UpdateScore()
+    {
+        if(numberOfRowsThisTurn > 0)
+        {
+            switch (numberOfRowsThisTurn)
+            {
+                case 1:
+                    ClearedOneLine();
+                    break;
+
+                case 2:
+                    ClearedTwoLines();
+                    break;
+
+                case 3:
+                    ClearedThreeLines();
+                    break;
+
+                case 4:
+                    ClearedFourLines();
+                    break;
+            }
+
+            numberOfRowsThisTurn = 0;
+        }
+    }
+
+    void Update()
+    {
+        if (currentScore < 1000)
+        {
+            FindObjectOfType<TetraminoScript>().fallSpeed = 1f;
+            fallSpeedGame = 1f;
+        }
+
+        if (currentScore > 1000)
+        {
+            FindObjectOfType<TetraminoScript>().fallSpeed = 0.5f;
+            fallSpeedGame = 0.5f;
+        }
+
+        if (currentScore > 5000)
+        {
+            FindObjectOfType<TetraminoScript>().fallSpeed = 0.3f;
+            fallSpeedGame = 0.3f;
+        }
+
+        if (currentScore > 15000)
+        {
+            FindObjectOfType<TetraminoScript>().fallSpeed = 0.1f;
+            fallSpeedGame = 0.1f;
+        }
+
+        UpdateUI();
+
+        UpdateScore();
+    }
+
+    public void ClearedOneLine()
+    {
+        currentScore += (scoreOneLine * ((int)(1 / fallSpeedGame)));
+    }
+
+    public void ClearedTwoLines()
+    {
+        currentScore += (scoreTwoLine * ((int)(1 / fallSpeedGame)));
+    }
+
+    public void ClearedThreeLines()
+    {
+        currentScore += (scoreThreeLine * ((int)(1 / fallSpeedGame)));
+    }
+
+    public void ClearedFourLines()
+    {
+        currentScore += (scoreFourLine * ((int)(1 / fallSpeedGame)));
+    }
+
+    public bool CheckIsAboveBorder(TetraminoScript tetramino)
+    {
+        for(int x = 1; x < borderWidth; ++x)
+        {
+            foreach (Transform mino in tetramino.transform)
+            {
+                Vector3 pos = Round(mino.position);
+
+                if(pos.y > borderLimit - 1)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public bool IsFullRowAt(int y) 
+    { 
+        for(int x = 1; x < borderWidth; ++x)
+        {
+            if (border[x, y] == null)
+            {
+                return false;
+            }
+        }
+
+        numberOfRowsThisTurn++;
+        
+        return true;
+    }
+
+    public void DeleteMinoAt(int y)
+    {
+        for (int x = 1; x < borderWidth; ++x)
+        {
+            Destroy(border[x, y].gameObject);
+
+            border[x, y] = null;
+        }
+    }
+
+    public void MoveRowDown(int y)
+    {
+        for (int x = 1; x < borderWidth; ++x)
+        {
+            if (border[x, y] != null)
+            {
+                border[x, y - 1] = border[x, y];
+
+                border[x, y] = null;
+
+                border[x, y - 1].position += new Vector3(0, -1, 0);
+            }
+        }
+    }
+
+    public void MoveAllRowsDown(int y)
+    {
+        for (int i = y; i < borderHeight; ++i)
+        {
+            MoveRowDown(i);
+        }
+    }
+
+    public void DeleteRow()
+    {
+        for(int y = 1; y < borderHeight; ++y)
+        {
+            if(IsFullRowAt(y))
+            {
+                DeleteMinoAt(y);
+
+                MoveAllRowsDown(y + 1);
+
+                --y;
+            }
+        }
+    }
+
+    public void UpdateBorder(TetraminoScript tetramino)
+    {
+        for(int y = 1; y < borderHeight; ++y)
+        {
+            for (int x = 1; x < borderWidth; ++x)
+            {
+                if (border[x, y] != null)
+                {
+                    if (border[x, y].parent == tetramino.transform)
+                    {
+                        border[x, y] = null; 
+                    }
+                }
+            }
+        }
+
+        foreach(Transform mino in tetramino.transform)
+        {
+            Vector2 pos = Round(mino.position);
+
+            if(pos.y < borderHeight)
+            {
+                border[(int)pos.x, (int)pos.y] = mino;
+            }
+        }
+    }
+
+    public Transform GetTransformAtBorderPosition(Vector2 pos)
+    {
+        if(pos.y > borderHeight)
+        {
+            return null;
+        }
+        else
+        {
+            return border[(int)pos.x, (int)pos.y];
+        }
+    }
+
+    public void SpawnNextTetramino()
+    {
+        GameObject nextTetramino = (GameObject)Instantiate(Resources.Load(GetRandomTetramino(), typeof(GameObject)), new Vector2(5.0f, 20.0f), Quaternion.identity);
+    }
+
+    public bool CheckIsInsideBorder(Vector2 pos)
+    {
+        return ((int)pos.x >= 1  && (int)pos.x < borderWidth && (int)pos.y >= 1);
+    }
+
+    public Vector2 Round (Vector2 pos)
+    {
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+    }
+
+    string GetRandomTetramino()
+    {
+        int randomTetramino = Random.Range(0, 6);
+
+        string randomTetraminoName = "Prefabs/Tetraminos/TetraminoT";
+
+        switch(randomTetramino)
+        {
+            case 0:
+                randomTetraminoName = "Prefabs/Tetraminos/TetraminoI";
+                break;
+
+            case 1:
+                randomTetraminoName = "Prefabs/Tetraminos/TetraminoJ";
+                break;
+
+            case 2:
+                randomTetraminoName = "Prefabs/Tetraminos/TetraminoL";
+                break;
+
+            case 3:
+                randomTetraminoName = "Prefabs/Tetraminos/TetraminoO";
+                break;
+
+            case 4:
+                randomTetraminoName = "Prefabs/Tetraminos/TetraminoT";
+                break;
+
+            case 5:
+                randomTetraminoName = "Prefabs/Tetraminos/TetraminoZ";
+                break;
+        }
+
+        return randomTetraminoName;
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+}
---
+++ Assets/Scripts/TetraminoScript.cs
@@ -0,0 +1,304 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+//using System.Numerics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TetraminoScript : MonoBehaviour
+{
+    public static event OnSwipeInput SwipeEvent;
+    public delegate void OnSwipeInput(Vector2 direction);
+
+    private Vector2 tapPosition;
+    private Vector2 swipeDelta;
+
+    private float deadZone = 80;
+
+    private bool isSwiping;
+    private bool isMobile;
+
+    float fall = 0;
+    public float fallSpeed = 1;
+
+    void Start()
+    {
+        isMobile = Application.isMobilePlatform;
+
+        SwipeEvent += OnSwipe;
+    }
+
+    private void OnSwipe(Vector2 direction)
+    {
+        throw new NotImplementedException();
+    }
+
+    void Update()
+    {
+        if (!isMobile)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                isSwiping = true;
+                tapPosition = Input.mousePosition;
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                ResetSwipe();
+            }
+        }
+        else
+        {
+            if (Input.touchCount > 0)
+            {
+                if (Input.GetTouch(0).phase == TouchPhase.Began)
+                {
+                    isSwiping = true;
+                    tapPosition = Input.GetTouch(0).position;
+                }
+                else if (Input.GetTouch(0).phase == TouchPhase.Canceled ||
+                    Input.GetTouch(0).phase == TouchPhase.Ended)
+                {
+                    ResetSwipe();
+                }
+            }
+        }
+
+        CheckSwipe();
+
+        MoveDown();
+
+        //CheckUserInput();
+    }
+
+    private void CheckSwipe()
+    {
+        swipeDelta = Vector2.zero;
+
+        if (isSwiping)
+        {
+            if (!isMobile && Input.GetMouseButton(0))
+            {
+                swipeDelta = (Vector2)Input.mousePosition - tapPosition;
+            }
+            else if (Input.touchCount > 0)
+            {
+                swipeDelta = Input.GetTouch(0).position - tapPosition;
+            }
+        }
+
+        if (swipeDelta.magnitude > deadZone)
+        {
+            if (SwipeEvent != null)
+            {
+                if (Mathf.Abs(swipeDelta.x) > Mathf.Abs(swipeDelta.y))
+                {
+                    if (swipeDelta.x > 0)
+                    {
+                        transform.position += new Vector3(1, 0);
+
+                        if (CheckIsValidPosition())
+                        {
+                            FindObjectOfType<GameScript>().UpdateBorder(this);
+                        }
+                        else
+                        {
+                            transform.position += new Vector3(-1, 0, 0);
+                        }
+                    }
+                    else
+                    {
+                        transform.position += new Vector3(-1, 0, 0);
+
+                        if (CheckIsValidPosition())
+                        {
+                            FindObjectOfType<GameScript>().UpdateBorder(this);
+                        }
+                        else
+                        {
+                            transform.position += new Vector3(1, 0, 0);
+                        }
+                    }
+                }
+                else
+                {
+                    if (swipeDelta.y > 0)
+                    {
+                        transform.Rotate(0, 0, 90);
+                        
+                        if (CheckIsValidPosition())
+                        {
+                            FindObjectOfType<GameScript>().UpdateBorder(this);
+                        }
+                        else
+                        {
+                            transform.Rotate(0, 0, -90);
+                        }
+                    }
+                    else if (swipeDelta.y < 0)
+                    {
+                        transform.position += new Vector3(0, -1, 0);
+
+                        if (CheckIsValidPosition())
+                        {
+                            FindObjectOfType<GameScript>().UpdateBorder(this);
+                        }
+                        else
+                        {
+                            transform.position += new Vector3(0, 1, 0);
+
+                            FindObjectOfType<GameScript>().DeleteRow();
+
+                            if (FindObjectOfType<GameScript>().CheckIsAboveBorder(this))
+                            {
+                                FindObjectOfType<GameScript>().GameOver();
+                            }
+
+                            FindObjectOfType<GameScript>().SpawnNextTetramino();
+
+                            enabled = false;
+                        }
+
+                        fall = Time.time;
+                    }
+                }
+            }
+            ResetSwipe();
+        }
+    }
+
+    private void ResetSwipe()
+    {
+        isSwiping = false;
+
+        tapPosition = Vector2.zero;
+        swipeDelta = Vector2.zero;
+    }
+
+    //void CheckUserInput()
+    //{
+
+    //    if (Input.GetKeyDown(KeyCode.RightArrow))
+    //    {
+    //        transform.position += new Vector3(1, 0);
+
+    //        if (CheckIsValidPosition())
+    //        {
+    //            FindObjectOfType<GameScript>().UpdateBorder(this);
+    //        }
+    //        else
+    //        {
+    //            transform.position += new Vector3(-1, 0, 0);
+    //        }
+    //    }
+
+    //    else if (Input.GetKeyDown(KeyCode.LeftArrow))
+    //    {
+    //        transform.position += new Vector3(-1, 0, 0);
+
+    //        if (CheckIsValidPosition())
+    //        {
+    //            FindObjectOfType<GameScript>().UpdateBorder(this);
+    //        }
+    //        else
+    //        {
+    //            transform.position += new Vector3(1, 0, 0);
+    //        }
+    //    }
+
+    //    else if (Input.GetKeyDown(KeyCode.UpArrow))
+    //    {
+    //        transform.Rotate(0, 0, 90);
+
+    //        if (CheckIsValidPosition())
+    //        {
+    //            FindObjectOfType<GameScript>().UpdateBorder(this);
+    //        }
+    //        else
+    //        {
+    //            transform.Rotate(0, 0, -90);
+    //        }
+    //    }
+
+    //    else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
+    //    {
+    //        transform.position += new Vector3(0, -1, 0);
+
+    //        if (CheckIsValidPosition())
+    //        {
+    //            FindObjectOfType<GameScript>().UpdateBorder(this);
+    //        }
+    //        else
+    //        {
+    //            transform.position += new Vector3(0, 1, 0);
+
+    //            FindObjectOfType<GameScript>().DeleteRow();
+
+    //            if (FindObjectOfType<GameScript>().CheckIsAboveBorder(this))
+    //            {
+    //                FindObjectOfType<GameScript>().GameOver();
+    //            }
+
+    //            FindObjectOfType<GameScript>().SpawnNextTetramino();
+
+    //            GameScript.currentScore += individualScore;
+
+    //            enabled = false;
+    //        }
+
+    //        fall = Time.time;
+    //    }
+    //}
+
+    void MoveDown()
+    {
+        if(Time.time - fall >= fallSpeed)
+        {
+            transform.position += new Vector3(0, -1, 0);
+
+            if (CheckIsValidPosition())
+            {
+                FindObjectOfType<GameScript>().UpdateBorder(this);
+            }
+            else
+            {
+                transform.position += new Vector3(0, 1, 0);
+
+                FindObjectOfType<GameScript>().DeleteRow();
+
+                if (FindObjectOfType<GameScript>().CheckIsAboveBorder(this))
+                {
+                    FindObjectOfType<GameScript>().GameOver();
+                }
+
+                FindObjectOfType<GameScript>().SpawnNextTetramino();
+
+                enabled = false;
+            }
+
+            fall = Time.time;
+        }
+    }
+
+    bool CheckIsValidPosition()
+    {
+        foreach (Transform mino in transform)
+        {
+            Vector2 pos = FindObjectOfType<GameScript>().Round(mino.position);
+
+            if (FindObjectOfType<GameScript>().CheckIsInsideBorder(pos) == false)
+            {
+                return false;
+            }
+
+            if (FindObjectOfType<GameScript>().GetTransformAtBorderPosition(pos) != null && FindObjectOfType<GameScript>().GetTransformAtBorderPosition(pos).parent != transform)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
